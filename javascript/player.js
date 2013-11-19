570067d7817574c3f2d1d7bca974f1b36ce76f95
Dayliss <h.g@smithsinla.com>
2013-11-18 17:02:22 -0800

    Update player.js

--- javascript/player.js
+++ javascript/player.js
@@ -1,15 +1,10 @@
 
 border.on('mousemove',function() {
   var mousePos = stage.getPointerPosition();
-  player.setRotation(Math.atan2(player.getY() - mousePos.y,player.getX() - mousePos.x) + Math.PI/6);
+  player.setRotation(Math.atan2(player.getY() - mousePos.y,player.getX() - mousePos.x));
   layer.draw();
-  line.setPoints([[player.getX(),player.getY()],[mousePos.x,mousePos.y]]);
 });
 
-var line = new Kinetic.Line({
-  points: [[player.getX(),player.getY()]],
-  stroke: 'black',
-  strokeWidth: rad/10,
+border.on('click',function() {
+  playerBody.setFill('red');
 });
-
-layer.add(line);
